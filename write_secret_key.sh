bf9c56999186f51a8e6a288cfa28f78631c863ce
jwatson <jwatson@newrelic.com>
2019-11-06 14:03:53 -0800

    step 1.15: spit out the md5, for funzieis

--- write_secret_key.sh
+++ write_secret_key.sh
@@ -4,6 +4,9 @@ cat <<EOF > secret_key.gpg
 $GPG_SIGNING_KEY
 EOF
 
+md5sum secret_key.gpg
+md5 secret_key.gpg
+
 echo "Created secreted key file: Here it is: "
 ls -la secret_key.gpg
 cat secret_key.gpg
