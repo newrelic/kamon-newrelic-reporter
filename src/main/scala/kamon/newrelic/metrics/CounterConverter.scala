3bd7a2903ab57592266a588e7c543c00ef274552
jwatson <jwatson@newrelic.com>
2019-11-07 11:25:55 -0800

    wip on testing the spec

--- src/main/scala/kamon/newrelic/metrics/CounterConverter.scala
+++ src/main/scala/kamon/newrelic/metrics/CounterConverter.scala
@@ -4,20 +4,24 @@ import com.newrelic.telemetry.Attributes
 import com.newrelic.telemetry.metrics.{Count, Metric}
 import kamon.metric.{Instrument, MetricSnapshot}
 import kamon.newrelic.TagSetToAttributes.addTags
+import org.slf4j.LoggerFactory
 
 class CounterConverter {
+  private val logger = LoggerFactory.getLogger(classOf[CounterConverter])
 
   def convert(start: Long, end: Long, counter: MetricSnapshot.Values[Long]): Seq[Metric] = {
     val name: String = counter.name
     val dimensionName = counter.settings.unit.dimension.name
     val magnitudeName = counter.settings.unit.magnitude.name
     val scaleFactor = counter.settings.unit.magnitude.scaleFactor
+
     val attributes = new Attributes()
       .put("description", counter.description)
       .put("dimensionName", dimensionName)
       .put("magnitudeName", magnitudeName)
       .put("scaleFactor", scaleFactor)
 
+    logger.debug("name: {} ; numberOfInstruments: {}", counter.name, counter.instruments.size)
     counter.instruments.map { inst: Instrument.Snapshot[Long] =>
       new Count(name, inst.value, start, end, addTags(Seq(inst.tags), attributes.copy()))
     }
--- src/test/scala/kamon/newrelic/metrics/NewRelicMetricsReporterSpec.scala
+++ src/test/scala/kamon/newrelic/metrics/NewRelicMetricsReporterSpec.scala
@@ -1,14 +1,43 @@
 package kamon.newrelic.metrics
 
-import com.newrelic.telemetry.metrics.MetricBatchSender
-import kamon.metric.{MetricSnapshot, PeriodSnapshot}
+import java.time.temporal.ChronoUnit
+import java.time.{Duration, Instant}
+
+import com.newrelic.telemetry.Attributes
+import com.newrelic.telemetry.metrics.{Count, Metric, MetricBatch, MetricBatchSender}
+import kamon.metric.Metric.Settings.ForValueInstrument
+import kamon.metric.{Instrument, MeasurementUnit, MetricSnapshot, PeriodSnapshot}
+import kamon.tag.TagSet
 import org.mockito.Mockito._
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.jdk.CollectionConverters._
+
 class NewRelicMetricsReporterSpec extends WordSpec with Matchers {
 
+  val expectedCommonAttributes: Attributes = new Attributes()
+    .put("service.name", "kamon.service")
+    .put("instrumentation.source", "kamon-agent")
+
+
   "The metrics reporter" should {
-    "should send some metrics" in {
+    "send some metrics" in {
+      val from: Instant = Instant.now()
+      val to = from.plus(100, ChronoUnit.MILLIS)
+      val settings: ForValueInstrument = new ForValueInstrument(MeasurementUnit.percentage, Duration.ZERO)
+      val tagSet: TagSet = TagSet.from(Map("foo" -> "bar"))
+      val inst1 = new Instrument.Snapshot[Long](tagSet, 55L)
+      val inst2 = new Instrument.Snapshot[Long](tagSet, 66L)
+
+      val expectedAttributes = new Attributes()
+        .put("foo", "bar")
+        .put("description", "flam")
+        .put("dimensionName", "percentage")
+        .put("magnitudeName", "percentage")
+        .put("scaleFactor", 1.0)
+      val flib1: Metric = new Count("flib", 55L, from.toEpochMilli, to.toEpochMilli, expectedAttributes)
+      val flib2: Metric = new Count("flib", 66L, from.toEpochMilli, to.toEpochMilli, expectedAttributes)
+      val expectedBatch: MetricBatch = new MetricBatch(Seq(flib1, flib2).asJavaCollection, expectedCommonAttributes)
 
       val sender = mock(classOf[MetricBatchSender])
 
@@ -18,9 +47,10 @@ class NewRelicMetricsReporterSpec extends WordSpec with Matchers {
       val reporter = new NewRelicMetricsReporter(sender)
       reporter.reportPeriodSnapshot(periodSnapshot)
 
+      verify(sender).sendBatch(expectedBatch)
     }
 
-    "can be reconfigured" in {
+    "be reconfigurable" in {
       fail("build me")
     }
   }
