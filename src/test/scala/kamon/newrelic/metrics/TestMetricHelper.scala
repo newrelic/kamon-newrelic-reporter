bb0aff7b2c97545cb20e6f00d8e14762f97edda2
jplumb <jplumb@newrelic.com>
2019-11-07 15:05:54 -0800

    cleanup test

--- src/test/scala/kamon/newrelic/metrics/TestMetricHelper.scala
+++ src/test/scala/kamon/newrelic/metrics/TestMetricHelper.scala
@@ -2,6 +2,7 @@ package kamon.newrelic.metrics
 
 import java.time.{Duration, Instant}
 
+import kamon.metric.MeasurementUnit.Dimension
 import kamon.metric.{Instrument, MeasurementUnit, Metric, MetricSnapshot}
 import kamon.tag.TagSet
 
@@ -22,5 +23,13 @@ object TestMetricHelper {
     val instrument1 = new Instrument.Snapshot[Long](tagSet, value1)
     val instrument2 = new Instrument.Snapshot[Long](tagSet, value2)
     MetricSnapshot.ofValues("flib", "flam", settings, Seq(instrument1, instrument2))
+  }
+
+  def buildGauge = {
+    val tagSet: TagSet = TagSet.from(Map("foo" -> "bar"))
+    val settings = Metric.Settings.ForValueInstrument(
+      new MeasurementUnit(Dimension.Information, new MeasurementUnit.Magnitude("finch", 11.0d)), Duration.ofMillis(12))
+    val inst = new Instrument.Snapshot[Double](tagSet, 15.6d)
+    new MetricSnapshot.Values[Double]("shirley", "another one", settings, Seq(inst))
   }
 }
